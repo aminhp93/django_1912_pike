fa5da43d2a358677ba5f3ff2f4d20df3b894474a
aminhp93 <minhpn.org.ec@gmail.com>
2017-02-27 09:03:59 -0800

    0 - First Commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Amin/Dropbox/project/project_django_1912pike/venv/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
