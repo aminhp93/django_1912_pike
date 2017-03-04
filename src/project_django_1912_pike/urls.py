0603e22191ae6735bd02674b4842319745eca93b
aminhp93 <minhpn.org.ec@gmail.com>
2017-03-04 13:15:21 -0800

    09 - Add Navbar and Bootstrap

--- src/project_django_1912_pike/urls.py
+++ src/project_django_1912_pike/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url, include
 from django.contrib import admin
 from .views import HomeView
@@ -24,3 +26,7 @@ urlpatterns = [
     url(r'^courses/', include('courses.urls', namespace='courses')),
     url(r'^videos/', include('videos.urls', namespace='videos')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
